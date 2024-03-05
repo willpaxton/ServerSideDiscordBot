53677926e0dae6f7a4fb961ab749ef2e50f4a7bd
willpaxton <43458264+willpaxton@users.noreply.github.com>
2024-03-04 21:26:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,10 +24,24 @@ async def ping(ctx):
 # this not work
 @bot.command()
 @commands.has_permissions(kick_members=True)
-async def kick(ctx, user: discord.Member):
-    
-    await user.kick()
+async def kick(ctx, user: discord.Member, *, reason: str):
+    print(reason)
+    await user.kick(reason=reason)
     await ctx.send(f"Kicked user {user}")
+
+@bot.command()
+@commands.has_permissions(kick_members=True)
+async def ban(ctx, user: discord.Member, *, reason: str):
+    try:
+        print(reason)
+        if reason == ".":
+            await user.ban(reason="No reason specified")
+        else:
+            await user.ban(reason=reason)
+        await ctx.send(f"Kicked user {user}")
+    except:
+        await ctx.send("Please provide a reason.")
+
 
 # this do work
 @bot.command()
@@ -40,7 +54,30 @@ async def kick2(ctx, user: discord.Member, *, reason: str):
         await user.kick(reason=reason)
         await ctx.send(f"**{user}** has been kicked for **{reason}**.")
 
+# this is a very bandaid solution
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(f"An error occured: {str(error)}")
 # insert commands here
+
+# ctx is <discord.ext.commands.context.Context object at 0x00000257BFB1D780>
+
+# @bot.event
+# async def on_message(ctx):
+#     print(ctx.content)
+#     await discord.message.channel.send("lol")
+    
+
+@bot.event  
+async def on_message(message):
+    msg = message.content
+    # iterate over list of "banned" words here
+    print(f"{message.content}, {message.author}")
+    if "hi" in message.content:
+        # delete message here
+        await message.channel.purge(limit = 1)
+        await message.channel.send(msg)
+
 
 bot.run('MTIxMjUzNjc1NDIzODc4MzUyOA.GY4SF-.6j-EfrpZTkEWyejBKbm-tNAoJsDG23hAbKpvrE')
 
